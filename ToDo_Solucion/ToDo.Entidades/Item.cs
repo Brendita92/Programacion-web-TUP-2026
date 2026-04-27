7ab5bbc044250dd422c1d5d42fcae9c3f7724dbd
Brenda92 <brendimachuca92@gmail.com>
2026-04-27 20:04:04 -0300

    ToDo Tests

--- ToDo_Solucion/ToDo.Entidades/Item.cs
+++ ToDo_Solucion/ToDo.Entidades/Item.cs
@@ -2,10 +2,24 @@
 {
     public class Item
     {
-
-
         public string Titulo { get; set; }
 
-        public bool Estado { get; set; }
+        private bool _estado;
+        
+        public bool Estado
+        {           //acciones antes de retornar el valor
+            get { return _estado; }
+                   //acciones antes de asignar el valor
+            set { _estado = value; }
+
+        }
+
+        public override string ToString()
+        {
+            //return "Comprar leche (Pendiente)";
+            return $"{Titulo} ({(Estado ? "Completo" : "Pendiente")})";
+        }
     }
+
+
 }
